e9eaa80fc2c7da76106e14f203b01bb41e19a790
Christoph Pageler <christoph.pageler@me.com>
2018-04-11 14:37:33 +0200

    Update JiraSwiftTests.swift

--- Tests/JiraSwiftTests/JiraSwiftTests.swift
+++ Tests/JiraSwiftTests/JiraSwiftTests.swift
@@ -20,7 +20,7 @@ class JiraSwiftTests: XCTestCase {
         let e = expectation(description: "searchExpecation")
         let jiraClient = Jira.Client(url: URL(string: "https://thepeaklab.atlassian.net")!,
                                      username: "c.pageler",
-                                     password: "BUD03mne!")
+                                     password: "")
         jiraClient.search(jql: "key in (EWE027-65, EWE038-3, EWE027-58)") { result in
             switch result {
             case .success(let result):
