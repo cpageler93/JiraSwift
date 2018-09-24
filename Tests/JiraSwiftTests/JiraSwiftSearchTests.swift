ac4fbff25958aabc29f3a1ce25642318c776f5f0
Robert <rf@thepeaklab.com>
2018-09-24 10:59:28 +0200

    Add OAuth Support

--- Tests/JiraSwiftTests/JiraSwiftSearchTests.swift
+++ Tests/JiraSwiftTests/JiraSwiftSearchTests.swift
@@ -34,4 +34,22 @@ class JiraSwiftSearchTests: XCTestCase {
         waitForExpectations(timeout: 15, handler: nil)
     }
 
+    func testClientSearchWithOAuthTokenShouldReturnSomeIssues() {
+        let e = expectation(description: "searchExpecationWithOAuth")
+        let jiraClient = Jira.Client(url: URL(string: "your_jira_url")!,
+                                     oAuthAccessToken: "oauth_token")
+
+        jiraClient.search(jql: "key in (EWE027-65, EWE038-3, EWE027-58)") { result in
+            switch result {
+            case .success(let result):
+                XCTAssertEqual(result.issues.count, 3)
+            case .failure:
+                XCTFail()
+            }
+            e.fulfill()
+        }
+
+        waitForExpectations(timeout: 15, handler: nil)
+    }
+
 }
